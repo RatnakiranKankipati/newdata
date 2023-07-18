c7e734c3a48ba8798e251ed44e2a59cdd795b225
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2023-07-18 01:10:02 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+const userroute = require("./Routes/userRoute")
+const app = express()
+const port = process.env.PORT || 7000
+
+app.use(cors())
+
+app.use(express.json())
+
+// users route
+app.use("/gaylordusers", userroute)
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGO_URI,()=>{
+    console.log("db is connected");
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("sever is connected")
+})
+// gaylordstatistics123
