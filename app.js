08fde1bad5af9f9177d90ca0811deab4a3ca707b
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2023-07-17 23:14:43 -0800

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ app.use(express.json())
 app.use("/gaylordusers",userroute)
 
 mongoose.set('strictQuery', false);
-
-mongoose.connect(process.env.MONGO_URI,()=>{
-    console.log("db is connected");
-})
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
 
 app.listen(process.env.PORT,()=>{
     console.log("sever is connected")
