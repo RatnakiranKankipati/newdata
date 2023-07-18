fab4070e24d0058643a4e4018792150a23d1e1e6
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2023-07-18 01:30:57 -0800

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -27,13 +27,7 @@ exports.signup = async (req, res) => {
                 message: "all the fields are required"
             })
         }
-        const alreadyuser = await userModel.findOne({ "email":email})
-        if (alreadyuser) {
-            return res.status(401).json({
-                status: "fail",
-                message: "someone have alreday used to the email please enter the new email"
-            })
-        }
+       
         const user = await userModel.create({
             name: name,
             email: email,
