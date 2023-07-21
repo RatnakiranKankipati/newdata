c3256d0374e8587cc19a14321543ec328f428af0
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2023-07-20 23:32:02 -0800

    Update columnModal.js

--- Models/columnModal.js
+++ Models/columnModal.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 
 const columnSchema = new mongoose.Schema({
+    _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
     Userid: {
         type: String,
         required: true,
