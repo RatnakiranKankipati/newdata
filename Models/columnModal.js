40743f5a19db33408d7fe1481610d08151ff9f04
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2023-07-20 22:35:44 -0800

    Create columnModal.js

---
+++ Models/columnModal.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose")
+
+const columnSchema = new mongoose.Schema({
+    Userid: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    WorkItemId: {
+        type: Boolean,
+        default:false
+    },
+    Input: {
+        type: Boolean,
+        default:false
+    },
+    Output: {
+        type: Boolean,
+        default:false
+    },
+    Log: {
+        type: Boolean,
+        default:false
+    },
+    Status: {
+        type: Boolean,
+        default:false
+    },
+    CreatedOn: {
+        type: Boolean,
+        default:false
+    },
+    JobId: {
+        type: Boolean,
+        default:false
+    },
+    EqpCount: {
+        type: Boolean,
+        default:false
+    },
+    SheetCount: {
+        type: Boolean,
+        default:false
+    },
+    Duration: {
+        type: Boolean,
+        default:false
+    },
+    ErrorType: {
+        type: Boolean,
+        default:false
+    }
+})
+
+
+const column = new mongoose.model("columns", columnSchema)
+
+module.exports = column
