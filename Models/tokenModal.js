0bad81ca64787b583f4704d85311118dcbaf1205
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2024-04-30 16:28:19 +0530

    Update tokenModal.js

--- Models/tokenModal.js
+++ Models/tokenModal.js
@@ -1,1 +1,22 @@
+const mongoose=require("mongoose")
 
+const tokenSchema=new mongoose.Schema({
+        accesstoken:{
+        type:String,
+        required:true
+    },
+    refreshtoken:{
+      type:String,
+        required:true
+    }
+},
+{timestamps:true} 
+)
+
+
+
+
+
+const tokenModel=new mongoose.model("tokens",tokenSchema)
+
+module.exports=tokenModel
