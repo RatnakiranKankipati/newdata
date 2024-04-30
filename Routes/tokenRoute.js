1f14b7bda22de5295fa7dc622162fb132cbf87af
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2024-04-30 16:59:10 +0530

    Update tokenRoute.js

--- Routes/tokenRoute.js
+++ Routes/tokenRoute.js
@@ -1,1 +1,11 @@
+const token=require("../Controllers/tokenController")
+const express=require("express")
+const router=express.Router()
+const authentication=require("../middleware/authentication")
 
+
+router.route("/createtoken").post(authentication,token.createToken)
+router.route("/gettoken").get(authentication,token.Findtoken)
+router.route("/edittoken/:id").patch(authentication,token.updateToken)
+
+module.exports=router
