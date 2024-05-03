1d29990293a40eb3d12be07e7b0f808a9d732cb9
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2024-05-03 14:27:24 +0530

    Update tokenModal.js

--- Models/tokenModal.js
+++ Models/tokenModal.js
@@ -12,6 +12,10 @@ const tokenSchema=new mongoose.Schema({
         expiretime: {
         type: Date,
         default: () => new Date(Date.now() + 55 * 60 * 1000) // Set default expiration time to current time + 60 minutes
+    },
+        refreshexpiretime: {
+        type: Date,
+        default: () => new Date(Date.now() + (6.5 * 24 * 60 * 60 * 1000))
     }
 },
 {timestamps:true} 
